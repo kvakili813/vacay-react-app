a9e5391f6d4654c9b161d368601bad1798dc144e
Kayvan <kvakili813@gmail.com>
2019-01-02 14:39:56 -0500

    Renders comment form

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -20,3 +20,18 @@ class Comments extends React.Component {
       </div>
     )
   }
+  render () {
+    return (
+      <div className='comments'>
+        {this.props.restaurantComments.map(this.renderComment)}
+        <form ref='commentForm' className='comment-form' onSubmit={this.handleSubmit}>
+          <input type='text' ref='author' placeholder='author' />
+          <input type='text' ref='comment' placeholder='comment' />
+          <input type='submit' hidden />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Comments
