744c23d7e23d5fb3fca4e77380c86b7e4aa6e758
Kayvan <kvakili813@gmail.com>
2019-01-02 15:46:46 -0500

    Creates reducer

---
+++ client/src/reducers/comments.js
@@ -0,0 +1,22 @@
+function vacationComments (state = [], action) {
+    switch (action.type) {
+      case 'ADD_COMMENT':
+        return [...state, {
+          user: action.author,
+          text: action.comment
+        }]
+      default:
+        return state
+    }
+  }
+  function comments (state = [], action) {
+    if (typeof action.id !== 'undefined') {
+      return {
+        ...state,
+        [action.id]: restaurantComments(state[action.id], action)
+      }
+    }
+    return state
+  }
+  
+  export default comments
