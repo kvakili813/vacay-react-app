5cf6519c0e8128dbe095964a7765e067195778be
Kayvan <kvakili813@gmail.com>
2019-01-02 00:23:18 -0500

    Implements react & redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// React & Redux stuff
+import React from 'react'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router-dom'
+import { render } from 'react-dom'
+import {persistStore, autoRehydrate} from 'redux-persist'
+import createHistory from 'history/createBrowserHistory'
+import thunk from 'redux-thunk'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
