ecb969212abca0327bba8e303c5837d1697a25ee
Kayvan <kvakili813@gmail.com>
2019-01-02 15:24:05 -0500

    Adds class

--- client/src/components/VacationList.js
+++ client/src/components/VacationList.js
@@ -4,4 +4,24 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import * as actionCreators from '../actions/actionCreators'
 
-import Restaurant from './Restaurant'
+import vacation from './vacation'
+
+class VacationList extends Component {
+    render () {
+      let list
+      const { vacations } = this.props.places
+      console.log(vacations)
+      if (vacations) {
+        list =
+          vacations.map((r, i) => <vacation {...this.props} key={i} i={r.id} vacation={r} />)
+      } else {
+        list = <p>Loading...</p>
+      }
+      return (
+        <div className='list'>
+          {list}
+        </div>
+      )
+    }
+  }
+  
