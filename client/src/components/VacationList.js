c031f7715ad8d936eacc4a21468d9a948bd317f8
Kayvan <kvakili813@gmail.com>
2019-01-02 15:21:45 -0500

    Creates component and adds imports

---
+++ client/src/components/VacationList.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import * as actionCreators from '../actions/actionCreators'
+
+import Restaurant from './Restaurant'
