4936e185f2e698e7dba19c3da9a056a62be79221
Kayvan <kvakili813@gmail.com>
2019-01-03 02:46:04 -0500

    Updates single component

--- client/src/components/Single.js
+++ client/src/components/Single.js
@@ -5,5 +5,19 @@ import { connect } from 'react-redux'
 import * as actionCreators from '../actions/actionCreators'
 
 // Components
-import Restaurant from './Restaurant'
+import Restaurant from './Vacation'
 import Comments from './Comments'
+
+class Single extends Component {
+    render () {
+      let single
+      if (this.props.places.vacations) {
+        const { id } = this.props.match.params
+        const i = this.props.places.vacations.findIndex((r) => r.place_id === id)
+        const vacation = this.props.places.vacations[i]
+        const vacationComments = this.props.comments[id] || []
+        single =
+          <div className='single'>
+            <vacation i={i} vacation={vacation} {...this.props} />
+            <Comments vacationComments={vacationComments} {...this.props} />
+          </div>
