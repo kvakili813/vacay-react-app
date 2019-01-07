422e1cbbbf4d4d6ad199ce0a27bf9f1b9072d2fd
Kayvan <kvakili813@gmail.com>
2019-01-07 02:27:48 -0500

    Final changes and fixes seed issue

--- client/src/data/vacations.js
+++ client/src/data/vacations.js
@@ -6,35 +6,21 @@ const vacations = {
       'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/J8KrToIKl_DvWk8AwmE3oA/o.jpg',
       'is_closed': false,
       'url': 'https://www.yelp.com/biz/painturos-lebanon?adjust_creative=OX_Rao29LAkeWmUjZqKaQQ&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=OX_Rao29LAkeWmUjZqKaQQ',
-      'review_count': 26,
-      'categories': [
-        {
-          'alias': 'pizza',
-          'title': 'Pizza'
-        }
-      ],
-      'rating': 4.5,
-      'coordinates': {
-        'latitude': 36.20893,
-        'longitude': -86.3035
-      },
-      'transactions': [],
-      'price': '$',
-      'location': {
-        'address1': '522 W Main St',
-        'address2': 'Ste B',
-        'address3': null,
-        'city': 'Lebanon',
-        'zip_code': '37087',
-        'country': 'US',
-        'state': 'TN',
-        'display_address': [
-          '522 W Main St',
-          'Ste B',
-          'Lebanon, TN 37087'
-        ]
-      }
+      
+      'id': 'coney-island',
+      'name': "Coney Island",
+      'image_url': 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Coney_Island_beach_and_amusement_parks_%28June_2016%29.jpg',
+      'is_closed': false,
+    
+      'id': 'vietnam',
+      'name': "Vietnam",
+      'image_url': 'https://en.wikipedia.org/wiki/Vietnam#/media/File:%C4%90i%E1%BB%87n_Th%C3%A1i_H%C3%B2a.JPG',
+      'is_closed': false,
+
+    
+    
+    
     }
-  ],
+  ]
 }
         export default vacations
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Place.create(name: "Coney Island")
-Place.create(name: "Brooksfield")
-Place.create(name: "Hawaii")
-Place.create(name: "England")
+Place.create(place_id: 'coney-island',
+                name: "Coney Island",
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Coney_Island_beach_and_amusement_parks_%28June_2016%29.jpg')
+                Place.create(place_id: 'manhattan',
+                name: "Manhattan",
+                image_url: 'https://bobridgesgallery.com/wp-content/uploads/2017/06/Lower-Manhattan.jpg')
+Place.create(place_id: 'vietnam',
+                name: "Vietnam",
+                image_url: 'https://cdn.tourradar.com/s3/tour/original/95481_99a760be.jpg')
+Place.create(place_id: 'hawaii',
+                name: "Hawaii",
+                image_url: 'https://www.myhawaii.com.au/wp-content/uploads/sites/13/2018/08/Hawaii-Landscape-Copy.jpg')
+Place.create(place_id: 'england',
+    name: "England",
+    image_url: 'http://seekadegree.com/wp-content/uploads/2018/08/england-fall-semester-abroad.jpg')
